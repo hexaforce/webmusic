f52744b143bf501230031662169a99072ad71986
relics9 <relics9@relics9-linux>
2018-07-14 07:08:35 +0900

    test

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -57,10 +57,33 @@ app.factory('x', ['$http', '$q', function ($http, $q) {
       search: search
     };
     return factory;
-
+    
+    var opts = {
+    		  lines: 13, // The number of lines to draw
+    		  length: 38, // The length of each line
+    		  width: 17, // The line thickness
+    		  radius: 45, // The radius of the inner circle
+    		  scale: 1, // Scales overall size of the spinner
+    		  corners: 1, // Corner roundness (0..1)
+    		  color: '#ffffff', // CSS color or array of colors
+    		  fadeColor: 'transparent', // CSS color or array of colors
+    		  speed: 1, // Rounds per second
+    		  rotate: 0, // The rotation offset
+    		  animation: 'spinner-line-fade-quick', // The CSS animation name for the lines
+    		  direction: 1, // 1: clockwise, -1: counterclockwise
+    		  zIndex: 2e9, // The z-index (defaults to 2000000000)
+    		  className: 'spinner', // The CSS class to assign to the spinner
+    		  top: '50%', // Top position relative to parent
+    		  left: '50%', // Left position relative to parent
+    		  shadow: '0 0 1px transparent', // Box-shadow for the lines
+    		  position: 'absolute' // Element positioning
+    		};
+    
     function init(scope) {
+    	var spinner = new Spinner(opts).spin(document.getElementById('button-addon'));
       $http.get("/search-music-list/" + scope.year, null).then(function (response) {
         scope.data = response.data;
+        spinner.spin();
       }, function (response) {
         console.log('ERROR:' + response.data);
       });
