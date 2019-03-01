090b08f03a8eb1bef2f0e9b474a873fb512dd611
tomokazu.tantaka <tantaka.tomokazu@gmail.com>
2019-03-02 00:11:58 +0900

    ff

--- api/src/main/java/io/hexaforce/webmusic/api/WebmusicAipApplication.java
+++ api/src/main/java/io/hexaforce/webmusic/api/WebmusicAipApplication.java
@@ -1,4 +1,4 @@
-package io.hexaforce.webmusic;
+package io.hexaforce.webmusic.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
